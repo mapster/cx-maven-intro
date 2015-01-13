ce8257fe9aa5fbc8af03c8e43f02dcb1daeb2d11
mapster <mapster@ndt-lan.no>
2015-01-13 20:17:42 +0100

    lagt til en eksempel ressurs

---
+++ maven-intro/src/main/java/no/computas/workshop/MyNotes.java
@@ -0,0 +1,32 @@
+package no.computas.workshop;
+
+import javax.print.attribute.standard.Media;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Enkel notatblokk.
+ */
+@Path("notes")
+public class MyNotes {
+
+    private static List<String> notes = new LinkedList<>();
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getNotes() {
+        String s = new String();
+        for(String note: notes) {
+            s += note + "\n";
+        }
+        return s;
+    }
+
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    public void addNote(String note) {
+        notes.add(note);
+    }
+}
