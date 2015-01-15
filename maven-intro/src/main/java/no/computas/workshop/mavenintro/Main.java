ffa428b507546410f27e688ac2c2c27f72ce95f2
mapster <mapster@ndt-lan.no>
2015-01-15 19:33:37 +0100

    fikset statisk ressurs sti

--- maven-intro/src/main/java/no/computas/workshop/mavenintro/Main.java
+++ maven-intro/src/main/java/no/computas/workshop/mavenintro/Main.java
@@ -14,7 +14,7 @@ import java.net.URI;
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/maven-intro";
+    public static final String BASE_URI = "http://localhost:8080/maven-intro/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
@@ -28,7 +28,7 @@ public class Main {
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
         HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc, false);
-        httpServer.getServerConfiguration().addHttpHandler(new StaticHttpHandler("/home/mapster/Development/repos/cx-maven-intro/maven-intro/src/main/webapp"), "");
+        httpServer.getServerConfiguration().addHttpHandler(new StaticHttpHandler("src/main/webapp"), "");
         httpServer.start();
         return httpServer;
     }
