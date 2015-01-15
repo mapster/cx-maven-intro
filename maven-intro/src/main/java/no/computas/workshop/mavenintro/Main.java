f4f9df6bcdcdea2db7af9d8127e043c135b981a0
mapster <mapster@ndt-lan.no>
2015-01-15 18:51:10 +0100

    lagt til webapp ressurs mappe. endret på ressurs paths

--- maven-intro/src/main/java/no/computas/workshop/mavenintro/Main.java
+++ maven-intro/src/main/java/no/computas/workshop/mavenintro/Main.java
@@ -1,6 +1,7 @@
 package no.computas.workshop.mavenintro;
 
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.server.StaticHttpHandler;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -13,20 +14,23 @@ import java.net.URI;
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/maven-intro/";
+    public static final String BASE_URI = "http://localhost:8080/maven-intro";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
      * @return Grizzly HTTP server.
      */
-    public static HttpServer startServer() {
+    public static HttpServer startServer() throws IOException {
         // create a resource config that scans for JAX-RS resources and providers
         // in no.computas.workshop package
         final ResourceConfig rc = new ResourceConfig().packages("no.computas.workshop.mavenintro");
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+        HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc, false);
+        httpServer.getServerConfiguration().addHttpHandler(new StaticHttpHandler("/home/mapster/Development/repos/cx-maven-intro/maven-intro/src/main/webapp"), "");
+        httpServer.start();
+        return httpServer;
     }
 
     /**
