b731b579582bd95dae9ea412f7f2139c8453cefc
Nicho <nicholas@paulik.no>
2015-02-02 21:30:10 +0100

    Kommentarer

--- maven-intro/src/main/java/no/computas/workshop/mavenintro/HtmlParserResource.java
+++ maven-intro/src/main/java/no/computas/workshop/mavenintro/HtmlParserResource.java
@@ -2,8 +2,8 @@ package no.computas.workshop.mavenintro;
 
 import nu.xom.ParsingException;
 import org.apache.xalan.xsltc.trax.SAX2DOM;
-import org.ccil.cowan.tagsoup.Parser;
-import org.jsoup.Jsoup;
+import org.ccil.cowan.tagsoup.Parser; //Hint!
+import org.jsoup.Jsoup; //Hint!
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Entities;
 import org.w3c.dom.Node;
@@ -32,19 +32,25 @@ public class HtmlParserResource {
     @POST
     @Produces("text/html")
     public String getHtml(@FormParam("input") String input) throws ParsingException, IOException, SAXException, ParserConfigurationException, TransformerException {
+        //Parser
         Parser parser = new Parser();
         parser.setFeature(Parser.namespacePrefixesFeature, false);
         parser.setFeature(Parser.namespacesFeature, false);
         parser.setFeature(Parser.xmlnsURIsFeature, false);
+
+        //Sax2dom gir oss Document Object Model (DOM) av HTML parset av Tagsoup
         SAX2DOM sax2dom = new SAX2DOM();
         parser.setContentHandler(sax2dom);
         parser.parse(new InputSource(new StringReader(input)));
         Node doc = sax2dom.getDOM();
+
+        //Gjør om DOM til en streng
         StringWriter writer = new StringWriter();
         Transformer transformer = TransformerFactory.newInstance().newTransformer();
         transformer.transform(new DOMSource(doc), new StreamResult(writer));
         String html = writer.toString().substring("<?xml version=\"1.0\" encoding=\"UTF-8\"?>".length());
 
+        //Parser med Jsoup for å gjøre om til et XHTML-dokument som er fint formatert
         Document document = Jsoup.parse(html);
         Document.OutputSettings out = new Document.OutputSettings();
         out.prettyPrint(true);
