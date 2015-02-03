6678e9240141fa7f87ae550b5a62493c14f32f61
mapster <mapster@ndt-lan.no>
2015-02-03 22:07:22 +0100

    fjernet legg til knapp. økt antall endepunkt. skrevet eksempel for endepunkt 1. lagt til lenker til alle libs i index.html

--- maven-intro/src/main/java/no/computas/workshop/mavenintro/Oppgave4Resource.java
+++ maven-intro/src/main/java/no/computas/workshop/mavenintro/Oppgave4Resource.java
@@ -13,57 +13,91 @@ public class Oppgave4Resource {
     @GET
     @Path("result1")
     public String getResultOne(@QueryParam("input") String input) {
-        String result = "sett resultat i denne strengen";
-
-        //
-        //  Legg til kode under her.
-        //
-        ////////////////////////////
-
-            //her
-
-        ////////////////////////////
-        //
-        //  Legg til kode over her.
-        //
+        String result = input.toUpperCase();
         return result;
     }
 
     @GET
     @Path("result2")
     public String getResultTwo(@QueryParam("input") String input) {
-        String result = "sett resultat i denne strengen";
+        String result = "result2: input="+input;
 
-        //
+        ////////////////////////////
         //  Legg til kode under her.
-        //
         ////////////////////////////
 
-            //her
-
-        ////////////////////////////
-        //
-        //  Legg til kode over her.
-        //
         return result;
     }
 
     @GET
     @Path("result3")
     public String getResultThree(@QueryParam("input") String input) {
-        String result = "sett resultat i denne strengen";
+        String result = "result3: input="+input;
 
-        //
+        ////////////////////////////
         //  Legg til kode under her.
-        //
         ////////////////////////////
 
-            //her
+        return result;
+    }
+
+    @GET
+    @Path("result4")
+    public String getResultFour(@QueryParam("input") String input) {
+        String result = "result4: input="+input;
 
         ////////////////////////////
-        //
-        //  Legg til kode over her.
-        //
+        //  Legg til kode under her.
+        ////////////////////////////
+
+        return result;
+    }
+
+    @GET
+    @Path("result5")
+    public String getResultFive(@QueryParam("input") String input) {
+        String result = "result5: input="+input;
+
+        ////////////////////////////
+        //  Legg til kode under her.
+        ////////////////////////////
+
+        return result;
+    }
+
+    @GET
+    @Path("result6")
+    public String getResultSix(@QueryParam("input") String input) {
+        String result = "result6: input="+input;
+
+        ////////////////////////////
+        //  Legg til kode under her.
+        ////////////////////////////
+
+        return result;
+    }
+
+    @GET
+    @Path("result7")
+    public String getResultSeven(@QueryParam("input") String input) {
+        String result = "result7: input="+input;
+
+        ////////////////////////////
+        //  Legg til kode under her.
+        ////////////////////////////
+
+        return result;
+    }
+
+    @GET
+    @Path("result8")
+    public String getResultEight(@QueryParam("input") String input) {
+        String result = "result8: input="+input;
+
+        ////////////////////////////
+        //  Legg til kode under her.
+        ////////////////////////////
+
         return result;
     }
 }
